4b903b1b8f994bbe797595b757921cd6267ac42f
Shilpa Modi <shilpamodi78910@gmail.com>
2024-06-30 12:42:37 +0530

    madlib

---
+++ madlib.py
@@ -0,0 +1,8 @@
+name=input("enter name:")
+n2=input("enter your favorite person:")
+n3=input("enter fav person:")
+
+
+print(f"{name} is so beautiful")
+print(name + " loves " + n2)
+print(f"hey {n2} , but {name} loves {n3} more than u ")
