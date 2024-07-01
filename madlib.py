f73166307acf7f9e78e6b6968a7b7f1aaa5cf75f
Shilpa Modi <shilpamodi78910@gmail.com>
2024-07-01 17:32:03 +0530

    Remove hello and madlib.py files

--- madlib.py
+++
@@ -1,8 +0,0 @@
-name=input("enter name:")
-n2=input("enter your favorite person:")
-n3=input("enter fav person:")
-
-
-print(f"{name} is so beautiful")
-print(name + " loves " + n2)
-print(f"hey {n2} , but {name} loves {n3} more than u ")
